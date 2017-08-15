bd642d123034edc96c2e743c1c53fd54162d0072
werdeveloperss <samdevelopers317@gmail.com>
2017-08-15 17:30:55 +0530

    Controllers->Demo.php, Views->demo.php, Also removed index.php

---
+++ application/controllers/Demo.php
@@ -0,0 +1,8 @@
+<?php
+
+class Demo extends CI_Controller{
+	public function index(){
+		$this->load->view('demo');
+	}
+}
+?>
---
+++ application/views/demo.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Demo Page</title>
+</head>
+<body>
+<h1>This is Demo page without index.php</h1>
+</body>
+</html>
